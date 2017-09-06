fad31ef85c3ca4ff4b8dd7ed1783c11551ff49ae
kof2001kop <kof2001kop@163.com>
2017-09-07 00:59:08 +0800

    Add files via upload

---
+++ qqsrx.php
@@ -0,0 +1,17 @@
+<?php
+
+	$ch = curl_init("https://kylehe.me/vifeng/podcast/audio/qqsrx.xml");
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);     
+
+	$ret = curl_exec($ch);
+	curl_close($ch);
+
+	$ret = str_replace('<link>http://v.ifeng.com/</link>', "", $ret);
+	$ret = str_replace('<enclosure url="', "<link>", $ret);
+	$ret = str_replace('" type="audio/m4a" length="12345"/>', "</link>", $ret);
+	
+	echo $ret;
+?>
