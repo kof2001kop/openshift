60723812e0db143eaf34b06abbcd0b07806c7a02
kof2001kop <kof2001kop@163.com>
2017-10-17 01:31:28 +0800

    Update kqsrx.php

--- kqsrx.php
+++ kqsrx.php
@@ -1,6 +1,6 @@
 <?php
 
-	$ch = curl_init("https://kylehe.me/vifeng/podcast/audio/qqsrx.xml");
+	$ch = curl_init("https://www.youtube.com/feeds/videos.xml?$_GET['sort']=$_GET['value']");
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -9,9 +9,9 @@
 	$ret = curl_exec($ch);
 	curl_close($ch);
 
-	$ret = str_replace('<link>http://v.ifeng.com/</link>', "", $ret);
-	$ret = str_replace('<enclosure url="', "<link>", $ret);
-	$ret = str_replace('" type=', "</link><enclosure type=", $ret);
-	
+	$ret = str_replace('<media:group>', "", $ret);
+	$ret = str_replace('</media:group>', "", $ret);
+	$ret = str_replace('media:thumbnail url', "img src", $ret);
+
 	echo $ret;
 ?>
