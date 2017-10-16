c75342ec7807d2d29effe4c511353715cec9ae9b
kof2001kop <kof2001kop@163.com>
2017-10-17 02:43:44 +0800

    Update youtubefeeds.php

--- youtubefeeds.php
+++ youtubefeeds.php
@@ -1,13 +1,13 @@
 <?php
-	
-	$ch = curl_init("https://www.youtube.com/feeds/videos.xml");
+	$sort = $_GET['sort'];
+	$value = $_GET['value'];
+	$url = 'https://www.youtube.com/feeds/videos.xml?'.$sort.'='.$value;
+
+	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);    
-
-	$post_data = array ($_GET['sort'] => $_GET['value']);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 
 	$ret = curl_exec($ch);
 	curl_close($ch);
