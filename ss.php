427e8a3a31aad384542379c088fcaa3e63c687d5
kof2001kop <kof2001kop@163.com>
2018-06-22 22:33:46 +0800

    Update ss.php

--- ss.php
+++ ss.php
@@ -1,5 +1,5 @@
 <?php
-	$url = 'https://tool.ssrshare.com/tool/free_ssr';
+	$url = 'https://www.ssrtool.com/tool/free_ssr';
 
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
