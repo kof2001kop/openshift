9cbc555afa043bda2eb90a355c0b006cb9e0dc0c
kof2001kop <kof2001kop@163.com>
2018-07-05 20:03:28 +0800

    Update ss.php

--- ss.php
+++ ss.php
@@ -14,7 +14,7 @@
         $ret = substr($ret, $beg + 12);
         $end = strpos($ret, '</code>');
         $ret = substr($ret, 0, $end);
-        $ret = 'c3NyOi8v' + $ret;
+        $ret = base64_encode('ssr://' + $ret);
 
 	echo $ret;
 ?>
