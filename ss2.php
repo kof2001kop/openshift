4f9057c493381b647ba494bcccbd8dd45b713109
kof2001kop <kof2001kop@163.com>
2018-07-05 20:44:32 +0800

    Update ss2.php

--- ss2.php
+++ ss2.php
@@ -1,14 +1,14 @@
 <?php
-	      $url = 'https://shadowsocks-share.herokuapp.com/subscribe?valid=1';
-	      $ch = curl_init($url);
+	$url = 'https://shadowsocks-share.herokuapp.com/subscribe?valid=1';
+	$ch = curl_init($url);
 	      
         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
-	      curl_setopt($ch, CURLOPT_HEADER, 0); 
-	      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 	     
         $ret = curl_exec($ch);
-	      curl_close($ch);
+	curl_close($ch);
 
         echo $ret;
 ?>
