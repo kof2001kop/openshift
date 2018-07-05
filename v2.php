13888c85dc6546b714d6e56aa4551d065e133f41
kof2001kop <kof2001kop@163.com>
2018-07-05 22:47:45 +0800

    Update v2.php

--- v2.php
+++ v2.php
@@ -15,7 +15,7 @@
         $end = strpos($ret, '</h5>');
         $ret = substr($ret, 0, $end);
 	$ret = $ret.'\n';
-	$ret = $ret + 'r';
+	$ret = $ret.'rfdg';
 	/*$url = 'https://us01-cdn.xn--e6qy70csvg.ml/';
 	$ch = curl_init($url);
 	      
