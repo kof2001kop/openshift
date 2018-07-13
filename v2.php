ba46cfc287e90d9457cb624ff702301cbd157b14
kof2001kop <kof2001kop@163.com>
2018-07-14 00:46:37 +0800

    Update v2.php

--- v2.php
+++ v2.php
@@ -48,9 +48,9 @@
         $ret3 = substr($ret3, $beg);
         $end = strpos($ret3, '"');
         $ret3 = substr($ret3, 0, $end);
-	/*$ret = $ret.$ret2;*/
+	$ret = $ret.$ret3;
 
         $ret = base64_encode($ret);
 
-        echo $ret.PHP_EOL.$ret3;
+        echo $ret;
 ?>
