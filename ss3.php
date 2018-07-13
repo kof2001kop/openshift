66496c28d73875c73cb4fbd7b2907b08c6cbf04f
kof2001kop <kof2001kop@163.com>
2018-07-14 00:52:04 +0800

    Create ss3.php

---
+++ ss3.php
@@ -0,0 +1,20 @@
+<?
+   $url = 'https://free.gyteng.com/';
+$ch = curl_init($url);
+	      
+       curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+
+        $ret3 = curl_exec($ch);
+	curl_close($ch);
+
+	$beg = strpos($ret3, 'ss://');
+        $ret3 = substr($ret3, $beg);
+        $end = strpos($ret3, '"');
+        $ret3 = substr($ret3, 0, $end);
+	$ret = $ret.$ret3;
+  
+  echo $ret;
+?>
