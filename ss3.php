62fa2982e6d4180704063f4587946b196a385f23
kof2001kop <kof2001kop@163.com>
2018-07-14 00:52:53 +0800

    Update ss3.php

--- ss3.php
+++ ss3.php
@@ -1,8 +1,8 @@
 <?
-   $url = 'https://free.gyteng.com/';
-$ch = curl_init($url);
+   	$url = 'https://free.gyteng.com/';
+	$ch = curl_init($url);
 	      
-       curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
@@ -14,7 +14,6 @@ $ch = curl_init($url);
         $ret3 = substr($ret3, $beg);
         $end = strpos($ret3, '"');
         $ret3 = substr($ret3, 0, $end);
-	$ret = $ret.$ret3;
   
-  echo $ret;
+  	echo $ret3;
 ?>
