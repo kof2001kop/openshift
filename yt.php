2330e0d5bc9055ba214d3bcd337a1565f4196150
kof2001kop <kof2001kop@163.com>
2018-07-31 12:48:35 +0800

    Update yt.php

--- yt.php
+++ yt.php
@@ -1,4 +1,14 @@
 <?php
-  $url = 'https://www.youtube.com/feeds/videos.xml?playlist_id='.$_GET['list'];
+    $url = 'https://www.youtube.com/feeds/videos.xml?playlist_id='.$_GET['list'];
+    $ch = curl_init($url);
+	      
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	  curl_setopt($ch, CURLOPT_HEADER, 0); 
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	     
+    $ret = curl_exec($ch);
+	  curl_close($ch);
+
 
 ?>
