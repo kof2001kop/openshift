fa4eee8d81c813ec60279d1dae428b42737f0886
kof2001kop <kof2001kop@163.com>
2018-07-31 12:55:05 +0800

    Update yt.php

--- yt.php
+++ yt.php
@@ -1,14 +1,17 @@
 <?php
-    $url = 'https://www.youtube.com/feeds/videos.xml?playlist_id='.$_GET['list'];
-    $ch = curl_init($url);
+	$url = 'https://www.youtube.com/feeds/videos.xml?playlist_id='.$_GET['list'];
+   	$ch = curl_init($url);
 	      
-    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
-	  curl_setopt($ch, CURLOPT_HEADER, 0); 
-	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 	     
-    $ret = curl_exec($ch);
-	  curl_close($ch);
+	$ret = curl_exec($ch);
+	curl_close($ch);
 
+	str_replace('<updated>', '<!--', $ret);
+	str_replace('</updated>', '-->', $ret);
 
+	echo $ret;
 ?>
