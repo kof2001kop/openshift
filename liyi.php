ff5e9acd4d83e6625e6f83f704e9120a0f2bce76
kof2001kop <kof2001kop@163.com>
2018-08-10 03:21:06 +0800

    Update liyi.php

--- liyi.php
+++ liyi.php
@@ -10,5 +10,22 @@
 	$ret = curl_exec($ch);
 	curl_close($ch);
 
+	$posBeg = strpos($ret, 'Authoritem') + 10;
+	$posBeg = strpos($ret, 'href=', $posBeg) + 6;
+	$posEnd = strpos($ret, '">', $posBeg);
+	$url = substr($ret, $posBeg, $posEnd - $posBeg);
 
+
+   	$ch = curl_init($url);
+	      
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	     
+	$ret = curl_exec($ch);
+	curl_close($ch);
+
+
+	echo $ret;
 ?>
