be66877ac0767d6bf21a9633c51bd834391bc182
kof2001kop <kof2001kop@163.com>
2018-08-10 03:16:19 +0800

    Update liyi.php

--- liyi.php
+++ liyi.php
@@ -1,5 +1,5 @@
 <?php
-	$url = 'https://www.youtube.com/feeds/videos.xml?playlist_id='.$_GET['key'];
+	$url = 'https://hk.news.appledaily.com/author/authorpaging/93/0/0';
    	$ch = curl_init($url);
 	      
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
