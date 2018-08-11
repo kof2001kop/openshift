da6ae02866578ad80295cd14ed0274115a34ddfa
kof2001kop <kof2001kop@163.com>
2018-08-11 12:17:14 +0800

    Update liyi.php

--- liyi.php
+++ liyi.php
@@ -26,6 +26,10 @@
 	$ret = curl_exec($ch);
 	curl_close($ch);
 
+	$posBeg = strpos($ret, '<p>');
+	$posEnd = strpos($ret, '</p>', $posBeg) + 4;
+	$content = substr($ret, $posBeg, $posEnd - $posBeg);
+
 	$head = '<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" media="screen" href="/~d/styles/rss2enclosuresfull.xsl"?><?xml-stylesheet type="text/css" media="screen" href="http://feeds.feedburner.com/~d/styles/itemcontent.css"?>
 <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
@@ -38,7 +42,7 @@
 <atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="hub" href="http://pubsubhubbub.appspot.com/" />
 <itunes:explicit>no</itunes:explicit><itunes:subtitle>李怡</itunes:subtitle>';
 
-	$content = '<item><title><![CDATA[在美国坐火车，主要目的不是出行……而是看风景]]></title><description><![CDATA[<div><div><div>
+	$content1 = '<item><title><![CDATA[在美国坐火车，主要目的不是出行……而是看风景]]></title><description><![CDATA[<div><div><div>
 <h2>美国的火车是什么样的？</h2><div><div>
 
 <p><img src="http://pic1.zhimg.com/70/v2-d1d777fd34f3bc83e360bf8b12140950_b.jpg" alt=""></p>
@@ -49,5 +53,5 @@
 <language>en-us</language><media:rating>nonadult</media:rating></channel></rss>
 ';
 
-	echo $ret;
+	echo $content;
 ?>
