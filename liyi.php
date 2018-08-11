1d1ae47739f98ec366dcb774003d0f70ddfd7147
kof2001kop <kof2001kop@163.com>
2018-08-11 14:50:12 +0800

    Update liyi.php

--- liyi.php
+++ liyi.php
@@ -50,25 +50,34 @@
 	$posEnd = strpos($ret, '</p>', $posBeg) + 4;
 	$content = substr($ret, $posBeg, $posEnd - $posBeg);
 
-	
 	$head = '<?xml version="1.0" encoding="UTF-8"?>
-<feed xmlns:yt="http://www.youtube.com/xml/schemas/2015" xmlns:media="http://search.yahoo.com/mrss/" xmlns="http://www.w3.org/2005/Atom">
- <title>李怡-蘋果專欄</title>
- <link rel="alternate" href="https://hk.appledaily.com/author/index/93"/>
- <author>
-  <name>李怡-蘋果專欄</name>
-  <uri>https://hk.appledaily.com/author/index/93</uri>
- </author>
- <published>2018-08-10T00:00:01+00:00</published>';
-
-	$contentNew = '<entry><title>'.$title.'</title>';
-  	$contentNew .= '<link rel="alternate" href="'.$url.'"/>';
-	$contentNew .= '<author><name>李怡</name></author>';
-	$contentNew .= '<description><![CDATA[<p><img src="'.$pic.'"></p><p>我啊是大勢多久阿卡</p>]]></description>';
-	$contentNew .= '<published>'.$date.'</published>';
-	$contentNew .= '<updated>'.$date.'</updated></entry></feed>';
-	/*$contentNew .= '<media:group><media:thumbnail url="'.$pic.'/>';
-	$contentNew .= '<media:description>'.$description.'</media:description></media:group></entry></feed>';*/
+		<?xml-stylesheet type="text/xsl" media="screen" href="/~d/styles/rss2enclosuresfull.xsl"?>
+		<?xml-stylesheet type="text/css" media="screen" href="http://feeds.feedburner.com/~d/styles/itemcontent.css"?>
+		<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+		<channel>
+		<title><![CDATA[李怡]]></title>
+		<description><![CDATA[李怡]]></description>
+		<link>'.$url.'</link>
+		<generator>RSS for Node</generator>
+		<lastBuildDate>.$date.</lastBuildDate>
+		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="self" type="application/rss+xml" href="http://feeds.feedburner.com/zhihu-daily" />
+		<feedburner:info xmlns:feedburner="http://rssnamespace.org/feedburner/ext/1.0" uri="zhihu-daily" />
+		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="hub" href="http://pubsubhubbub.appspot.com/" />
+		<itunes:explicit>no</itunes:explicit>
+		<itunes:subtitle>李怡</itunes:subtitle>';
+	
+	$contentNew = '<item>
+			<title><![CDATA['.$title.']]></title>
+			<description><![CDATA['.$pic.$content.']]></description>
+			<link>'.$url.'</link>
+			<guid isPermaLink="true">'.$url.'</guid>
+			<pubDate>'.$date.'</pubDate>
+			</item>
+			<language>en-us</language>
+			<media:rating>nonadult</media:rating>
+			</channel>
+			</rss>
+			';
 		
 	echo $head.$contentNew;
 ?>
