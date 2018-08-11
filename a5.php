c71423d2e0fb095d6633ef899b8a9509b6d8fb4c
kof2001kop <kof2001kop@163.com>
2018-08-11 15:22:38 +0800

    Update and rename a4.php to a5.php

--- a5.php
+++ a5.php
@@ -38,6 +38,11 @@
 	$posEnd = strpos($ret, '"', $posBeg);
 	$pic = substr($ret, $posBeg, $posEnd - $posBeg);
 
+	$posBeg = strpos($ret, '<a href="/author');
+	$posBeg = strpos($ret, '>', $posBeg) + 1;
+	$posEnd = strpos($ret, '</a>', $posBeg);
+	$author = substr($ret, $posBeg, $posEnd - $posBeg);
+
 	$posBeg = strpos($ret, 'description" content=\'') + 22;
 	$posEnd = strpos($ret, '\' />', $posBeg);
 	$description = substr($ret, $posBeg, $posEnd - $posBeg);
@@ -59,8 +64,8 @@
 		<?xml-stylesheet type="text/css" media="screen" href="http://feeds.feedburner.com/~d/styles/itemcontent.css"?>
 		<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
 		<channel>
-		<title><![CDATA[李怡]]></title>
-		<description><![CDATA[李怡]]></description>
+		<title><![CDATA['.$author.']]></title>
+		<description><![CDATA['.$author.']]></description>
 		<link>'.$url.'</link>
 		<generator>RSS for Node</generator>
 		<lastBuildDate>.$date.</lastBuildDate>
@@ -68,7 +73,7 @@
 		<feedburner:info xmlns:feedburner="http://rssnamespace.org/feedburner/ext/1.0" uri="zhihu-daily" />
 		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="hub" href="http://pubsubhubbub.appspot.com/" />
 		<itunes:explicit>no</itunes:explicit>
-		<itunes:subtitle>李怡</itunes:subtitle>';
+		<itunes:subtitle>'.$author.'</itunes:subtitle>';
 	
 	$contentNew = '<item>
 			<title><![CDATA['.$title.']]></title>
