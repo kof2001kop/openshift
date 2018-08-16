1fbd9ff0932fec4359940c364230422024143e28
kof2001kop <kof2001kop@163.com>
2018-08-16 23:51:31 +0800

    Update polymerhk.php

--- polymerhk.php
+++ polymerhk.php
@@ -42,13 +42,12 @@
 	}
 	$date = $year.'-'.strval($i).'-'.$day.'T00:00:01+00:00';
 
-/*	$posBeg = strpos($ret, 'imagecover');
+	$posBeg = strpos($ret, 'imagecover');
 	$posBeg = strpos($ret, 'src="', $posBeg) + 5;
 	$posEnd = strpos($ret, '"', $posBeg);
 	$pic = substr($ret, $posBeg, $posEnd - $posBeg);
 
-	$posBeg = strpos($ret, 'author vcard">') + 14;
-	$posBeg = strpos($ret, '>', $posBeg) + 1;
+	$posBeg = strpos($ret, 'rel="author">') + 13;
 	$posEnd = strpos($ret, '</a>', $posBeg);
 	$author = substr($ret, $posBeg, $posEnd - $posBeg);
 
@@ -56,8 +55,9 @@
 	$posEnd = strpos($ret, '</h1>', $posBeg);
 	$title = substr($ret, $posBeg, $posEnd - $posBeg);
 
-	$posBeg = strpos($ret, '<div class="entry-content">');
-	$posEnd = strpos($ret, '<div id="wpdevar_comment_1', $posBeg) + 1;
+	$posBeg = strpos($ret, 'postcontentwrap');
+	$posBeg = strpos($ret, '<p>', $posBeg);
+	$posEnd = strpos($ret, '<div class="sharedaddy', $posBeg) + 1;
 	$content = substr($ret, $posBeg, $posEnd - $posBeg);
 	$content .= '/div>';
 
@@ -66,12 +66,12 @@
 		<?xml-stylesheet type="text/css" media="screen" href="http://feeds.feedburner.com/~d/styles/itemcontent.css"?>
 		<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
 		<channel>
-		<title>哲學</title>
-		<description>好青年荼毒室</description>
+		<title>聚言時報</title>
+		<description>polymer</description>
 		<link>'.$url.'</link>
 		<generator>RSS for Node</generator>
 		<lastBuildDate>.$date.</lastBuildDate>
-		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="self" type="application/rss+xml" href="https://corrupttheyouth.net" />
+		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="self" type="application/rss+xml" href="http://polymerhk.com" />
 		<feedburner:info xmlns:feedburner="http://rssnamespace.org/feedburner/ext/1.0" uri="apple-daily" />
 		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="hub" href="http://pubsubhubbub.appspot.com/" />
 		<itunes:explicit>no</itunes:explicit>
@@ -90,6 +90,5 @@
 			</rss>
 			';
 		
-	echo $head.$contentNew;*/
-	echo $date;
+	echo $head.$contentNew;
 ?>
