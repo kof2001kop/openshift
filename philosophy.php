7fd55b20a6eb6aa93e84536fcea95bec065318ff
kof2001kop <kof2001kop@163.com>
2018-08-16 19:03:04 +0800

    Update philosophy.php

--- philosophy.php
+++ philosophy.php
@@ -9,11 +9,11 @@
 	     
 	$ret = curl_exec($ch);
 	curl_close($ch);
-	/*$posBeg = strpos($ret, '<article') + 8;
+	$posBeg = strpos($ret, '<article') + 8;
 	$posBeg = strpos($ret, 'href=', $posBeg) + 6;
 	$posEnd = strpos($ret, '"', $posBeg);
 	$url = substr($ret, $posBeg, $posEnd - $posBeg);
-   	$ch = curl_init($url);
+   	/*$ch = curl_init($url);
 	      
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
@@ -82,5 +82,5 @@
 			';
 		
 	echo $head.$contentNew;*/
-	echo $ret;
+	echo $url;
 ?>
