b2e1624c5a3b1ed265c7148c0249981f683c0dba
kof2001kop <kof2001kop@163.com>
2018-08-16 22:00:11 +0800

    Update polymerhk.php

--- polymerhk.php
+++ polymerhk.php
@@ -9,11 +9,12 @@
 	     
 	$ret = curl_exec($ch);
 	curl_close($ch);
-	$posBeg = strpos($ret, '<article') + 8;
-	$posBeg = strpos($ret, 'href=', $posBeg) + 6;
+	$posBeg = strpos($ret, '最新文章');
+	$posBeg = strpos($ret, '<h2 class="title">', $posBeg);
+	$posBeg = strpos($ret, 'href="', $posBeg) + 6;
 	$posEnd = strpos($ret, '"', $posBeg);
 	$url = substr($ret, $posBeg, $posEnd - $posBeg);
-   	$ch = curl_init($url);
+   /*	$ch = curl_init($url);
 	      
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
@@ -80,5 +81,6 @@
 			</rss>
 			';
 		
-	echo $head.$contentNew;
+	echo $head.$contentNew;*/
+	echo $url;
 ?>
