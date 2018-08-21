77ff4e1bfe5c4160357f071aedc5bff673ce5255
kof2001kop <kof2001kop@163.com>
2018-08-21 19:26:04 +0800

    Update and rename xizuo.php to xizuo1.php

--- xizuo1.php
+++ xizuo1.php
@@ -1,5 +1,5 @@
 <?php
-	$url = 'https://www.webtoons.com/zh-hant/comedy/fengren/list?title_no=692&page=1';
+	$url = 'https://www.webtoons.com/zh-hant/comedy/fengren/list?title_no='.$_GET['num'].'&page=1';
    	$ch = curl_init($url);
 	      
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
