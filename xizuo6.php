59e9b81c55d230dc0b74ef4ff5f036bde41149b5
kof2001kop <kof2001kop@163.com>
2018-08-21 22:15:10 +0800

    Update xizuo6.php

--- xizuo6.php
+++ xizuo6.php
@@ -62,10 +62,10 @@
 			$link = str_replace('http', 'https', $link);
 		
 		//open connection
-		//$ch = curl_init($link);
+		$ch = curl_init($link);
 	      
-		//$header = array('Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-		/*		'Accept-Encoding:gzip, deflate, br',
+		$header = array('Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+				'Accept-Encoding:gzip, deflate, br',
 				'Accept-Language: en-GB,en;q=0.5',
 			        'Connection: keep-alive',
 			        'Upgrade-Insecure-Requests: 1',
@@ -79,8 +79,8 @@
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 
-		$result = curl_exec($ch);*/
-		echo $link;
+		$result = curl_exec($ch);
+		echo $result;
 		//file_put_contents(strval($i).'.jpg', fopen($result, 'r'));
 		//$contentNew .= '<img src="http://openshift-163.a3c1.starter-us-west-1.openshiftapps.com/'.strval($i).'.jpg">';
 		$i++;
