fbdcaf133adf5e529094156c9329ba03a4140917
kof2001kop <kof2001kop@163.com>
2018-08-24 23:58:51 +0800

    Update manga.php

--- manga.php
+++ manga.php
@@ -24,22 +24,11 @@
 	     
 	$ret = curl_exec($ch);
 	curl_close($ch);
-	$posBeg = strpos($ret, 'class="day">') + 12;
-	$posEnd = strpos($ret, '<', $posBeg);
-	$day = trim(substr($ret, $posBeg, $posEnd - $posBeg));
-	$posBeg = strpos($ret, 'class="month">') + 14;
-	$posEnd = strpos($ret, '<', $posBeg);
-	$month = trim(substr($ret, $posBeg, $posEnd - $posBeg));
-	$posBeg = strpos($ret, 'class="year">') + 13;
-	$posEnd = strpos($ret, '<', $posBeg);
-	$year = trim(substr($ret, $posBeg, $posEnd - $posBeg));
-	$monthArray = array('一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月');
-	$i = 1;
-	foreach ($monthArray as &$value)
-	{
-		if ($value === $month) break;
-		else $i++;
-	}
+	$posBeg = strpos($ret, '发表时间：');
+	$posEnd = strpos($ret, '</span>', $posBeg);
+	$date = trim(substr($ret, $posBeg, $posEnd - $posBeg));
+	$date = str_replace('发表时间：', '', $date);
+	$date = str_replace(' ', '', $date);
 	$date = $year.'-'.strval($i).'-'.$day.'T00:00:01+00:00';
 
 	$posBeg = strpos($ret, 'imagecover');
