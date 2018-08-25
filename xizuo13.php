f6cd808772ea4471c5bfd88e59594e8d04f321c1
kof2001kop <kof2001kop@163.com>
2018-08-25 17:32:14 +0800

    Update xizuo13.php

--- xizuo13.php
+++ xizuo13.php
@@ -32,20 +32,6 @@
 	$posEnd = strpos($ret, '<', $posBeg);
 	$category = substr($ret, $posBeg, $posEnd - $posBeg);
 
-	$posBeg = strpos($ret, '_listUl');
-	$posBeg = strpos($ret, 'href="', $posBeg) + 6;
-	$posEnd = strpos($ret, '"', $posBeg);
-	$url = substr($ret, $posBeg, $posEnd - $posBeg);
-   	$ch = curl_init($url);
-	      
-	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
-	curl_setopt($ch, CURLOPT_HEADER, 0); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-	     
-	$ret = curl_exec($ch);
-	curl_close($ch);
-
 	$posBeg = strpos($ret, '<p class="summary">');
 	$posEnd = strpos($ret, '</p>', $posBeg) + 4;
 	$content = substr($ret, $posBeg, $posEnd - $posBeg);
