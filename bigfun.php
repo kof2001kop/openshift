1862126768421b17aa87b47afad5d1e6ab9f5de4
kof2001kop <kof2001kop@163.com>
2018-08-25 19:45:30 +0800

    Update bigfun.php

--- bigfun.php
+++ bigfun.php
@@ -1,1 +1,13 @@
+<?php
+$url = 'https://www.webtoons.com/zh-hant/comedy/fengren/list?title_no='.$_GET['num'].'&page=1';
+   	$ch = curl_init($url);
+	      
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	     
+	$ret = curl_exec($ch);
+	curl_close($ch);
 
+?>
