6d98d037be28821f64670747ed69fb298db432ea
kof2001kop <kof2001kop@163.com>
2018-08-25 22:29:34 +0800

    Update bigfun.php

--- bigfun.php
+++ bigfun.php
@@ -32,20 +32,6 @@
 	$contentNew = str_replace(']]></title>', '</title>', $contentNew);
 	$contentNew = str_replace('<guid>https://www.bilibili.com/video/', '<guid>https://kpr-rss.herokuapp.com/bigfunvideo.php?id=', $contentNew);
 
-/*	$ch = curl_init('https://www.parsevideo.com/api.php?callback=jQuery112404553552822525099_1535204029430&url=https://www.bilibili.com/video/av30175740&hash=f62bb240eec1ac36738b9e2ccb31400d');
-	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
-	curl_setopt($ch, CURLOPT_HEADER, 0); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-	     
-	$ret = curl_exec($ch);
-	curl_close($ch);
-
-	$posBeg = strpos($ret, '[{"url":"') + 9;
-	$posEnd = strpos($ret, '"', $posBeg);
-	$url = substr($ret, $posBeg, $posEnd - $posBeg);
-	$url = str_replace('\\', '', $url);
-*/
 	echo $contentNew;
 
 ?>
