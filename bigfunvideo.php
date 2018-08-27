595db6bcb723370b1e48b68008e78dc5d8b480ce
kof2001kop <kof2001kop@163.com>
2018-08-27 17:56:41 +0800

    Update bigfunvideo.php

--- bigfunvideo.php
+++ bigfunvideo.php
@@ -1,5 +1,17 @@
 <?php
-//f62bb240eec1ac36738b9e2ccb31400d
+	$url = 'https://www.parsevideo.com/#https://www.bilibili.com/video/'.$_GET['id'];
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	$ret = curl_exec($ch);
+	curl_close($ch);
+	$posBeg = strpos($ret, 'hash = "') + 8;
+	$posEnd = strpos($ret, '"', $posBeg);
+	$hash = substr($ret, $posBeg, $posEnd - $posBeg);
+	
+/*
 	$ch = curl_init('https://www.parsevideo.com/api.php?callback=jQuery112404553552822525099_'.time().'430&url=https://www.bilibili.com/video/'.$_GET['id'].'&hash=9f2be91361f1b5dc7f6400c8a8508685');
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
@@ -14,6 +26,6 @@
 	$url = str_replace('\\', '', $url);
 
 	//header('Location:'.$url);
-   	//exit;
-echo $ret;
+   	//exit;*/
+echo $hash;
 ?>
