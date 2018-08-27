cfec40bf9763c87dc14f72f5cdca5dbd3d17a0d6
kof2001kop <kof2001kop@163.com>
2018-08-27 17:58:18 +0800

    Update bigfunvideo.php

--- bigfunvideo.php
+++ bigfunvideo.php
@@ -11,8 +11,8 @@
 	$posEnd = strpos($ret, '"', $posBeg);
 	$hash = substr($ret, $posBeg, $posEnd - $posBeg);
 	
-/*
-	$ch = curl_init('https://www.parsevideo.com/api.php?callback=jQuery112404553552822525099_'.time().'430&url=https://www.bilibili.com/video/'.$_GET['id'].'&hash=9f2be91361f1b5dc7f6400c8a8508685');
+
+	$ch = curl_init('https://www.parsevideo.com/api.php?callback=jQuery112404553552822525099_'.time().'430&url=https://www.bilibili.com/video/'.$_GET['id'].'&hash='.$hash);
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -25,7 +25,6 @@
 	$url = substr($ret, $posBeg, $posEnd - $posBeg);
 	$url = str_replace('\\', '', $url);
 
-	//header('Location:'.$url);
-   	//exit;*/
-echo $hash;
+	header('Location:'.$url);
+   	exit;
 ?>
