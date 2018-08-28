ea8da83c4ad22d9d76786cbd6a8e1cdec55e58f6
kof2001kop <kof2001kop@163.com>
2018-08-28 21:49:20 +0800

    Update epochtimes.php

--- epochtimes.php
+++ epochtimes.php
@@ -9,11 +9,32 @@
 	     
 	$ret = curl_exec($ch);
 	curl_close($ch);
+
+	$url = Array();
+	$pic = Array();
+	$title = Array();
+	$date = Array();
+
 	$posBeg = strpos($ret, '<article>');
-	$posBeg = strpos($ret, '<h2 class="title">', $posBeg);
 	$posBeg = strpos($ret, 'href="', $posBeg) + 6;
 	$posEnd = strpos($ret, '"', $posBeg);
-	$url = substr($ret, $posBeg, $posEnd - $posBeg);
+	$url[] = 'http://hk.epochtimes.com'.substr($ret, $posBeg, $posEnd - $posBeg);
+
+	$posBeg = strpos($ret, '<article>');
+	$posBeg = strpos($ret, 'src="', $posBeg) + 5;
+	$posEnd = strpos($ret, '"', $posBeg);
+	$pic[] = substr($ret, $posBeg, $posEnd - $posBeg);
+
+	$posBeg = strpos($ret, '<article>');
+	$posBeg = strpos($ret, 'title">', $posBeg) + 7;
+	$posEnd = strpos($ret, '<', $posBeg);
+	$title[] = substr($ret, $posBeg, $posEnd - $posBeg);
+
+	$posBeg = strpos($ret, '<article>');
+	$posBeg = strpos($ret, 'article-date">', $posBeg) + 14;
+	$posEnd = strpos($ret, '<', $posBeg);
+	$date[] = trim(substr($ret, $posBeg, $posEnd - $posBeg)).'T00:00:01+00:00';
+
    	$ch = curl_init($url);
 	      
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
@@ -23,36 +44,11 @@
 	     
 	$ret = curl_exec($ch);
 	curl_close($ch);
-	$posBeg = strpos($ret, 'class="day">') + 12;
-	$posEnd = strpos($ret, '<', $posBeg);
-	$day = trim(substr($ret, $posBeg, $posEnd - $posBeg));
-	$posBeg = strpos($ret, 'class="month">') + 14;
-	$posEnd = strpos($ret, '<', $posBeg);
-	$month = trim(substr($ret, $posBeg, $posEnd - $posBeg));
-	$posBeg = strpos($ret, 'class="year">') + 13;
-	$posEnd = strpos($ret, '<', $posBeg);
-	$year = trim(substr($ret, $posBeg, $posEnd - $posBeg));
-	$monthArray = array('一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月');
-	$i = 1;
-	foreach ($monthArray as &$value)
-	{
-		if ($value === $month) break;
-		else $i++;
-	}
-	$date = $year.'-'.strval($i).'-'.$day.'T00:00:01+00:00';
 
-	$posBeg = strpos($ret, 'imagecover');
-	$posBeg = strpos($ret, 'src="', $posBeg) + 5;
-	$posEnd = strpos($ret, '"', $posBeg);
-	$pic = substr($ret, $posBeg, $posEnd - $posBeg);
 
 	$posBeg = strpos($ret, 'rel="author">') + 13;
 	$posEnd = strpos($ret, '</a>', $posBeg);
 	$author = substr($ret, $posBeg, $posEnd - $posBeg);
-
-	$posBeg = strpos($ret, 'entry-title">') + 13;
-	$posEnd = strpos($ret, '</h1>', $posBeg);
-	$title = substr($ret, $posBeg, $posEnd - $posBeg);
 
 	$posBeg = strpos($ret, 'postcontentwrap');
 	$posBeg = strpos($ret, '<p>', $posBeg);
@@ -64,12 +60,12 @@
 		<?xml-stylesheet type="text/css" media="screen" href="http://feeds.feedburner.com/~d/styles/itemcontent.css"?>
 		<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
 		<channel>
-		<title>聚言時報</title>
-		<description>polymer</description>
-		<link>'.$url.'</link>
+		<title>大紀元</title>
+		<description>epochtimes</description>
+		<link>http://hk.epochtimes.com</link>
 		<generator>RSS for Node</generator>
 		<lastBuildDate>'.$date.'</lastBuildDate>
-		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="self" type="application/rss+xml" href="http://polymerhk.com" />
+		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="self" type="application/rss+xml" href="http://hk.epochtimes.com" />
 		<feedburner:info xmlns:feedburner="http://rssnamespace.org/feedburner/ext/1.0" uri="apple-daily" />
 		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="hub" href="http://pubsubhubbub.appspot.com/" />
 		<itunes:explicit>no</itunes:explicit>
