d383aaa5c421ed0e89dc77d1832322d1dc3f516a
kof2001kop <kof2001kop@163.com>
2018-08-28 22:26:09 +0800

    Update epochtimes.php

--- epochtimes.php
+++ epochtimes.php
@@ -62,7 +62,9 @@
 	$posEnd = strpos($ret, '</section>', $posBeg);
 	$content[$k] = substr($ret, $posBeg, $posEnd - $posBeg);
 	$content[$k] = str_replace('◇', '', $content[$k]);
-		
+	$content[$k] = str_replace('<p> </p>', '', $content[$k]);
+	$content[$k] = str_replace('<p></p>', '', $content[$k]);
+
 	$k++;
 	}
 
