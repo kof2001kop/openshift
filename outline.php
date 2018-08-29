6641793cfcf0ba9c81721b1446ce4dc77d8679b5
kof2001kop <kof2001kop@163.com>
2018-08-29 21:17:57 +0800

    Update outline.php

--- outline.php
+++ outline.php
@@ -1,6 +1,13 @@
 <?php
 
-
+$ch = curl_init('https://www.parsevideo.com/api.php?callback=jQuery112404553552822525099_'.time().'430&url=https://www.bilibili.com/video/'.$_GET['id'].'&hash='.$hash);
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	     
+	$ret = curl_exec($ch);
+	curl_close($ch);
 
 
 
