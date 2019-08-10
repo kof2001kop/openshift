01439dc53fdf3a67143632616dc84902bf51301a
kof2001kop <kof2001kop@163.com>
2019-08-10 20:34:29 +0800

    update

--- readmoo.php
+++ readmoo.php
@@ -1,5 +1,5 @@
 <?php
-	$url = 'https://news.readmoo.com/category/reading-and-life/digest/';
+	$url = 'https://hkug.arukascloud.io/topics/2?type=daily';
    	$ch = curl_init($url);
 	      
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
@@ -10,7 +10,6 @@
 	curl_close($ch);
 
 	$url = Array();
-	$pic = Array();
 	$title = Array();
 	$date = Array();
 	$author = Array();
@@ -19,11 +18,15 @@
 	$i = 0;
 	while ($i < 3)
 	{
-	$posBeg = strpos($ret, '<article');
-	$posBeg = strpos($ret, '<h2 class="page-title">', $posBeg);
+	$posBeg = strpos($ret, '<h4');
 	$posBeg = strpos($ret, 'href="', $posBeg) + 6;
 	$posEnd = strpos($ret, '"', $posBeg);
-	$url[] = substr($ret, $posBeg, $posEnd - $posBeg);
+	$url[] = 'https://hkug.arukascloud.io'.substr($ret, $posBeg, $posEnd - $posBeg);
+	
+	$posBeg = strpos($ret, '>', $posEnd) + 1;
+	$posEnd = strpos($ret, '</a>', $posBeg);
+	$title[] = substr($ret, $posBeg, $posEnd - $posBeg);
+	
 	$ret = substr($ret, $posEnd);
 	$i++;
 	}
@@ -40,46 +43,21 @@
 	$ret = curl_exec($ch);
 	curl_close($ch);
 
-	$posBeg = strpos($ret, 'datetime="') + 10;
-	$posEnd = strpos($ret, '"', $posBeg);
+	$posBeg = strpos($ret, 'anticon anticon-clock-circle-o c0126"></i>') + 42;
+	$posEnd = strpos($ret, '<', $posBeg);
 	$date[$k] = substr($ret, $posBeg, $posEnd - $posBeg);
 
-	$posBeg = strpos($ret, 'og:image" content="') + 19;
-	$posEnd = strpos($ret, '"', $posBeg);
-	$pic[$k] = substr($ret, $posBeg, $posEnd - $posBeg);
 
-	$posBeg = strpos($ret, 'text-align: right;">文');
-	$posEnd = strpos($ret, '</', $posBeg);
-	$author = substr($ret, $posBeg, $posEnd - $posBeg);
-	$author[$k] = str_replace('text-align: right;">文／', '', $author[$k]);
-	$author[$k] = str_replace('text-align: right;">文╱', '', $author[$k]);
-	$author[$k] = str_replace('text-align: right;">文/', '', $author[$k]);
-
-
-	$posBeg = strpos($ret, 'og:title" content="') + 19;
-	$posEnd = strpos($ret, '"', $posBeg);
-	$title[$k] = substr($ret, $posBeg, $posEnd - $posBeg);
-	$title[$k] = str_replace('「', '', $title[$k]);
-	$title[$k] = str_replace('」', '', $title[$k]);
-
-
-	$posBeg = strpos($ret, 'text-align: right;">');
-	$posBeg = strpos($ret, '</', $posBeg) + 2;
+	$posBeg = strpos($ret, 'c0123 ">') + 8;
 	$posBeg = strpos($ret, '>', $posBeg) + 1;
-	$posEnd = strpos($ret, '<div class="book-inside"', $posBeg);
-	$content[$k] = trim(substr($ret, $posBeg, $posEnd - $posBeg));
-	if (strpos(substr($content[$k], strrpos($content[$k], '<a')), '立即前往試讀►►►'))
-	{
-		$endStr = substr($content[$k], strrpos($content[$k], '</a>') + 4);
-		$content[$k] = trim(substr($content[$k], 0, strrpos($content[$k], '<a')));
-		
-		$content[$k] .= 'kof2001kopkpr';
-		$content[$k] = str_replace('，kof2001kopkpr', '', $content[$k]);
-		$content[$k] = str_replace('kof2001kopkpr', '', $content[$k]);
+	$posEnd = strpos($ret, '<', $posBeg);
+	$author[$k] = substr($ret, $posBeg, $posEnd - $posBeg);
 
-		$content[$k] .= $endStr;
-	}
-	
+
+	$posBeg = strpos($ret, 'c0115">') + 7;
+	$posEnd = strpos($ret, '<i class="anticon anticon-tag-o c0126">', $posBeg);
+	$content[$k] = trim(substr($ret, $posBeg, $posEnd - $posBeg));
+
 	$k++;
 	}
 
@@ -88,9 +66,9 @@
 		<?xml-stylesheet type="text/css" media="screen" href="http://feeds.feedburner.com/~d/styles/itemcontent.css"?>
 		<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
 		<channel>
-		<title>好書秒讀</title>
-		<description>ReadMoo</description>
-		<link>https://news.readmoo.com</link>
+		<title>連登</title>
+		<description>LIHKG</description>
+		<link>https://lihkg.com</link>
 		<generator>RSS for Node</generator>
 		<lastBuildDate>'.$date[0].'</lastBuildDate>
 		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="self" type="application/rss+xml" href="https://news.readmoo.com" />
@@ -105,7 +83,7 @@
 	{
 	$contentNew .= '<item>
 			<title>'.$title[$j].'</title>
-			<description><![CDATA['.'<img src="'.$pic[$j].'">'.$content[$j].']]></description>
+			<description><![CDATA['.$content[$j].']]></description>
 			<link>'.$url[$j].'</link>
 			<guid isPermaLink="true">'.$url[$j].'</guid>
 			<pubDate>'.$date[$j].'</pubDate>
