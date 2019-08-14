4e76b10785f0bab192a16ff5b64151cf1673092f
kof2001kop <kof2001kop@163.com>
2019-08-14 16:12:53 +0800

    Update and rename rssbig7.php to rssbig8.php

--- rssbig8.php
+++ rssbig8.php
@@ -1,12 +1,26 @@
 <?php
-   	$ch = curl_init($_GET['url']);
+   	$ch = curl_init('https://rsshub.app/telegram/channel/politicJokeZhao');
 	      
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-	     
 	$ret = curl_exec($ch);
+	
+	$posBeg = strrpos($ret, '</item>') + 7;
+	$ret = substr($ret, 0, $posBeg);
+	
+	$ch = curl_init('https://rsshub.app/instagram/user/hkgoldenforum');
+	      
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	$ret1 = curl_exec($ch);
+	
+	$posBeg = strpos($ret1, '<item>');
+	$ret .= substr($ret1, $posBeg);
+	
 	curl_close($ch);
 
 	$ret = str_replace('<description><![CDATA[', '<description><![CDATA[<big>', $ret);
