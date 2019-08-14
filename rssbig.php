4bcfe2685e0905ed65d642487dc4ef8a68e0b8a0
kof2001kop <kof2001kop@163.com>
2019-08-14 09:18:25 +0800

    Update and rename chenglap.php to rssbig.php

---
+++ rssbig.php
@@ -0,0 +1,16 @@
+<?php
+   	$ch = curl_init($_GET['url']);
+	      
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	     
+	$ret = curl_exec($ch);
+	curl_close($ch);
+
+	$date = str_replace('<description>', '<description><big><big>', $date);
+	$date = str_replace('</description>', '</big></big></description>', $date);
+		
+	echo $date;
+?>
