133ad1d3e858828b26ecc9bfca1ec169f97bf8b4
kof2001kop <kof2001kop@163.com>
2019-08-14 09:26:33 +0800

    i

--- rssbig.php
+++ rssbig.php
@@ -1,5 +1,5 @@
 <?php
-   	$ch = curl_init($_GET['url']);
+   	$ch = curl_init('https://'.$_GET['url']);
 	      
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
@@ -12,5 +12,6 @@
 	$date = str_replace('<description>', '<description><big><big>', $date);
 	$date = str_replace('</description>', '</big></big></description>', $date);
 		
+	echo $_GET['url'];
 	echo $date;
 ?>
