471e68920d20f6e951160d4e43798d211a0eab1c
kof2001kop <kof2001kop@163.com>
2019-08-15 13:01:36 +0800

    tj

--- hkug21.php
+++ hkug21.php
@@ -48,14 +48,14 @@
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 	$ret2 = curl_exec($ch);
-	 
+/*	 
 	$ch = curl_init($url[$k].'&page=3');
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 	$ret3 = curl_exec($ch);
-/*
+
         if (strpos($ret2, '<div class="ant-list-empty-text">') === FALSE)
         {
 
