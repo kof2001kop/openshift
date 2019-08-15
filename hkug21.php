a79f9c040278822a03faf2b0bcf246605aff0762
kof2001kop <kof2001kop@163.com>
2019-08-15 13:32:10 +0800

    hj

--- hkug21.php
+++ hkug21.php
@@ -37,15 +37,22 @@
         //1、初始化一个批处理handle
         $mh = curl_multi_init();
 
-    foreach ($urls as $i => $url) 
-{
-    $conn[$i] = curl_init($url);
-    curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");	
-    curl_setopt($conn[$i], CURLOPT_HEADER, 0);   
-    curl_setopt($conn[$i], CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt($conn[$i], CURLOPT_RETURNTRANSFER, true);
-    curl_multi_add_handle($mh, $conn[$i]);
-}
+        foreach ($urls as $i => $url) 
+        {
+        $conn[$i] = curl_init($url);
+        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");	
+        curl_setopt($conn[$i], CURLOPT_HEADER, 0);   
+        curl_setopt($conn[$i], CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($conn[$i], CURLOPT_RETURNTRANSFER, true);
+        curl_multi_add_handle($mh, $conn[$i]);
+        }
+
+        //3、并发执行，直到全部结束。
+        do 
+        {
+        curl_multi_exec($mh, $active);
+        } 
+        while ($active);
 
 
         while ($k < $i)
