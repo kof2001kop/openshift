88de705810dfa5dc65a74b9b4c65f2e4062b5ba4
kof2001kop <kof2001kop@163.com>
2019-08-15 08:02:10 +0800

    qw

--- test.php
+++ test.php
@@ -1,6 +1,21 @@
 <?php
 
-$sp = '';
+$sp = '<script type="text/javascript">
+const accounts = [
+    ["CCCAT", "https://cccat.io/user/login.php", "kof2001kopkpr@gmail.com", "nmpvvg"]
+]
+async function launch() 
+{
+    for (var i in accounts) 
+    {
+        let title = accounts[i][0]
+        let url = accounts[i][1]
+        let email = accounts[i][2]
+        let password = accounts[i][3]
+        await login(url, email, password, title)
+    }
+    $done();
+}';
 
 $out = '<!DOCTYPE html>
 <html>
