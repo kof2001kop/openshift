adb2e0b73fa19c3bde767bc33c66d5996651b1bd
kof2001kop <kof2001kop@163.com>
2019-08-15 10:13:38 +0800

    hj

--- hkug21.php
+++ hkug21.php
@@ -40,7 +40,7 @@
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);	     
 	$ret = curl_exec($ch);
-
+/*
 	$ch = curl_init($url[$k].'&page=2');
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
@@ -57,7 +57,7 @@
 	
         if (strpos($ret2, '<div class="ant-list-empty-text">') === FALSE)
         {
-/*
+
         $posBeg = strpos($ret, '</div></div></div><div class="c0122">');
 	$ret = substr($ret, 0, $posBeg);
 	
