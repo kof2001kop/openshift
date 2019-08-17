ad290a522ca0b07424747c68acf9d1c1512e84a9
kof2001kop <kof2001kop@163.com>
2019-08-17 13:14:24 +0800

    u

--- hkgolden.php
+++ hkgolden.php
@@ -119,6 +119,45 @@
 
         curl_multi_close($mh);
 
+        $head = '<?xml version="1.0" encoding="UTF-8"?>
+		<?xml-stylesheet type="text/xsl" media="screen" href="/~d/styles/rss2enclosuresfull.xsl"?>
+		<?xml-stylesheet type="text/css" media="screen" href="http://feeds.feedburner.com/~d/styles/itemcontent.css"?>
+		<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+		<channel>
+		<title>高登</title>
+		<description>HKGOLDEN</description>
+		<link>https://hkgolden.com</link>
+		<generator>RSS for Node</generator>
+		<lastBuildDate>'.'no'.'</lastBuildDate>
+		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="self" type="application/rss+xml" href="https://news.readmoo.com" />
+		<feedburner:info xmlns:feedburner="http://rssnamespace.org/feedburner/ext/1.0" uri="apple-daily" />
+		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="hub" href="http://pubsubhubbub.appspot.com/" />
+		<itunes:explicit>no</itunes:explicit>
+		<itunes:subtitle>'.$author[0].'</itunes:subtitle>';
+	
+	$contentNew = '';
+	$j = 0;
+        $linkCur = 0;
+	while ($j < $lineSum)	
+	{
+	$contentNew .= '<item>
+			<title>'.$title[$j].'</title>
+			<description><![CDATA['.$content[$j].']]></description>
+			<link>'.$urls[$linkCur].'</link>
+			<guid isPermaLink="true">'.$urls[$linkCur].'</guid>
+			<pubDate>'.$urls[$linkCur].'</pubDate>
+			</item>';
+		
+            $j++;
+            $linkCur += 3;
+	}
 
+	$contentNew .= '<language>en-us</language>
+			<media:rating>nonadult</media:rating>
+			</channel>
+			</rss>
+			';
+		
+	echo $head.$contentNew;
 
 ?>
