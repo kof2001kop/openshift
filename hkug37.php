e86943512dc6b12c6261eeeb8afaea2d18925f68
kof2001kop <kof2001kop@163.com>
2019-08-17 08:33:37 +0800

    Update and rename hkug36.php to hkug37.php

--- hkug37.php
+++ hkug37.php
@@ -15,7 +15,7 @@
 	$author = Array();
 	$content = Array();
 
-        $lineSum = 10;
+        $lineSum = 2;
         $pageSum = 2;//深度，3頁足夠
 
 	$i = 0;
@@ -81,6 +81,12 @@
             $ret2 = $retArr[$i + 1];
             $ret3 = $retArr[$i + 2];
            
+            $posBegIMG = strpos($ret, '<img src="data');
+            $posEndIMG = strpos($ret, '>', $posBegIMG) + 1;
+            $headc = substr($ret, 0, $posBegIMG);
+            $tailc = substr($ret, $posEndIMG);
+            $ret = $headc.$tailc;
+	    
             if (strpos($ret2, '<div class="ant-list-empty-text">') === FALSE)
             {
 
