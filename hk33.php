34b6413d6fe1c2444c7598076450246aa0737517
kof2001kop <kof2001kop@163.com>
2019-08-18 15:43:54 +0800

    Update and rename hk32.php to hk33.php

--- hk33.php
+++ hk33.php
@@ -89,8 +89,8 @@
            
             
 
-            if (strpos($ret2, '<div class="post"') === TRUE)
-            {
+          //  if (strpos($ret2, '<div class="post"') === TRUE)
+          //  {
 
                 $posBeg = strpos($ret, 'form name="aspnetForm" ');
 	        $ret = substr($ret, 0, $posBeg);
@@ -110,7 +110,7 @@
 	
 	        $ret .= $ret3;
                 }
-            }
+          //  }
 
             $ret = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $ret);
             $ret = preg_replace('#<ul>(.*?)</ul>#is', '', $ret);
