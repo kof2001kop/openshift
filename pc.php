a626e5c4207d62e9b07688a2ebbfb5cf604ab06c
kof2001kop <kof2001kop@163.com>
2019-08-18 20:02:44 +0800

    Update pc.php

--- pc.php
+++ pc.php
@@ -41,10 +41,82 @@
 	$i++;
 	}
 	
-foreach ($urls as $i => $url) 
+        //1、初始化一个批处理handle
+        $mh = curl_multi_init();
+
+        foreach ($urls as $i => $url) 
         {
-echo $title[$i].'<br/>';
-echo $urls[$i].'<br/>';
-echo $date[$i].'<br/>';
-}
+        $conn[$i] = curl_init($url);
+        curl_setopt($conn[$i], CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");	
+        curl_setopt($conn[$i], CURLOPT_HEADER, 0);   
+        curl_setopt($conn[$i], CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($conn[$i], CURLOPT_RETURNTRANSFER, true);
+        curl_multi_add_handle($mh, $conn[$i]);
+        }
+
+        //3、并发执行，直到全部结束。
+        do 
+        {
+        curl_multi_exec($mh, $active);
+        } 
+        while ($active);
+
+        //4、获取结果
+        $retArr = Array();
+        foreach ($urls as $i => $url)  
+        {
+            $retArr[$i] = curl_multi_getcontent($conn[$i]);
+        }
+
+        foreach ($retArr as $i => $ret)  
+        {
+            $content[] = $ret;
+        }
+
+        foreach ($urls as $i => $url) {
+        curl_multi_remove_handle($mh,$conn[$i]);
+        curl_close($conn[$i]);
+        }
+
+        curl_multi_close($mh);
+
+	$head = '<?xml version="1.0" encoding="UTF-8"?>
+		<?xml-stylesheet type="text/xsl" media="screen" href="/~d/styles/rss2enclosuresfull.xsl"?>
+		<?xml-stylesheet type="text/css" media="screen" href="http://feeds.feedburner.com/~d/styles/itemcontent.css"?>
+		<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+		<channel>
+		<title>品葱</title>
+		<description>PINCONG</description>
+		<link>https://pincong.rocks</link>
+		<generator>RSS for Node</generator>
+		<lastBuildDate>'.'no'.'</lastBuildDate>
+		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="self" type="application/rss+xml" href="https://news.readmoo.com" />
+		<feedburner:info xmlns:feedburner="http://rssnamespace.org/feedburner/ext/1.0" uri="apple-daily" />
+		<atom10:link xmlns:atom10="http://www.w3.org/2005/Atom" rel="hub" href="http://pubsubhubbub.appspot.com/" />
+		<itunes:explicit>no</itunes:explicit>
+		<itunes:subtitle>'.'no'.'</itunes:subtitle>';
+	
+	$contentNew = '';
+	$j = 0;
+	while ($j < $lineSum)	
+	{
+	$contentNew .= '<item>
+			<title>'.$title[$j].'</title>
+			<description><![CDATA['.$content[$j].']]></description>
+			<link>'.$urls[$j].'</link>
+			<guid isPermaLink="true">'.$urls[$j].'</guid>
+			<pubDate>'.$date[$j].'</pubDate>
+			</item>';
+		
+            $j++;
+	}
+
+	$contentNew .= '<language>en-us</language>
+			<media:rating>nonadult</media:rating>
+			</channel>
+			</rss>
+			';
+		
+	echo $head.$contentNew;
+
 ?>
