d79d8c67181f2b7f192bd56df93fd69bb9164ff3
kof2001kop <kof2001kop@163.com>
2019-08-18 19:22:28 +0800

    Update and rename test.php to pc.php

---
+++ pc.php
@@ -0,0 +1,19 @@
+<?php
+        $url = 'https://pincong.rocks/feed';
+   	$ch = curl_init($url);
+	      
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	$ret = curl_exec($ch);
+	curl_close($ch);
+
+	$urls = Array();
+	$title = Array();
+	$date = Array();
+	$author = Array();
+	$content = Array();
+
+
+?>
