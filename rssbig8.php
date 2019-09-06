ded156d0432cb43d9af5fb1daecc83d4cc09217a
kof2001kop <kof2001kop@163.com>
2019-09-06 16:24:21 +0800

    Update and rename rssbig9.php to rssbig8.php

--- rssbig8.php
+++ rssbig8.php
@@ -1,5 +1,5 @@
 <?php
-   	$ch = curl_init('https://rsshubkpr.herokuapp.com/telegram/channel/politicJokeZhao');
+   	$ch = curl_init('https://rsshub.app/telegram/channel/politicJokeZhao');
 	      
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");
 	curl_setopt($ch, CURLOPT_HEADER, 0); 
