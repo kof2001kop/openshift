7b8d1d55decb8cbfaa9a41e50f4d4896c4a86450
kof2001kop <kof2001kop@163.com>
2019-09-25 10:07:21 +0800

    ui

--- frv.php
+++ frv.php
@@ -20,6 +20,17 @@
 	$ret2 = curl_exec($ch);
 	curl_close($ch);
 
+        $url = 'https://free-ss.site/v/tmp.json';
+   	$ch = curl_init($url);
+	      
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	$ret3 = curl_exec($ch);
+	curl_close($ch);
+
+
         $v = '"v": "2",';
 
         $host = '"host": "",';
@@ -104,6 +115,8 @@
 	$posEnd = strpos($ret, ',', $posBeg);
 	$aid = '"aid": "'.substr($ret, $posBeg, $posEnd - $posBeg).'",';
 	
+
+
         $uri = '{'.$v.$ps.$add.$port.$id.$aid.$net.$type.$host.$path.$tls.'}';
         $encodeURI2 = 'vmess://'.base64_encode($uri);
         
