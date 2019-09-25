1d986bdb6572a55439407670df780626aaa95466
kof2001kop <kof2001kop@163.com>
2019-09-25 09:38:02 +0800

    end

--- frv.php
+++ frv.php
@@ -62,5 +62,51 @@
         $uri = '{'.$v.$ps.$add.$port.$id.$aid.$net.$type.$host.$path.$tls.'}';
         $encodeURI = 'vmess://'.base64_encode($uri);
         
-        echo $encodeURI;
+        
+        $ret = $ret2;
+
+        $v = '"v": "2",';
+
+        $host = '"host": "",';
+
+        $type = '"type": "none",';
+
+        $posBeg = strpos($ret, 'security": "') + 12;
+	$posEnd = strpos($ret, '"', $posBeg);
+	$tls = '"tls": "'.substr($ret, $posBeg, $posEnd - $posBeg).'"';
+	
+        $posBeg = strpos($ret, 'path": "') + 8;
+	$posEnd = strpos($ret, '"', $posBeg);
+	$path = '"path": "'.substr($ret, $posBeg, $posEnd - $posBeg).'",';
+	
+        $posBeg = strpos($ret, 'network": "') + 11;
+	$posEnd = strpos($ret, '"', $posBeg);
+	$net = '"net": "'.substr($ret, $posBeg, $posEnd - $posBeg).'",';
+	
+        $posBeg = strpos($ret, 'id": "') + 6;
+	$posEnd = strpos($ret, '"', $posBeg);
+	$id = '"id": "'.substr($ret, $posBeg, $posEnd - $posBeg).'",';
+	
+        $posBeg = strpos($ret, 'address":');
+        $posBeg = strpos($ret, 'port": ', $posBeg) + 7;
+	$posEnd = strpos($ret, ',', $posBeg);
+        $portM = substr($ret, $posBeg, $posEnd - $posBeg);
+	$port = '"port": "'.$portM.'",';
+	
+        $posBeg = strpos($ret, 'address": "') + 11;
+	$posEnd = strpos($ret, '"', $posBeg);
+	$addM = substr($ret, $posBeg, $posEnd - $posBeg);
+        $add = '"add": "'.$addM.'",';
+	
+        $ps = '"ps": "'.$addM.':'.$portM.'",';
+
+        $posBeg = strpos($ret, 'alterId": ') + 10;
+	$posEnd = strpos($ret, ',', $posBeg);
+	$aid = '"aid": "'.substr($ret, $posBeg, $posEnd - $posBeg).'",';
+	
+        $uri = '{'.$v.$ps.$add.$port.$id.$aid.$net.$type.$host.$path.$tls.'}';
+        $encodeURI2 = 'vmess://'.base64_encode($uri);
+        
+        $rss = base64_encode($encodeURI.'<br/>'.$encodeURI2);
+        echo $rss;
 ?>
