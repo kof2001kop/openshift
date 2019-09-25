ee3fa4f60eac1f5d5953f3bc0324130ac5750ced
kof2001kop <kof2001kop@163.com>
2019-09-25 08:00:12 +0800

    Update and rename checkin.html to frv.php

---
+++ frv.php
@@ -0,0 +1,26 @@
+<?php
+        $url = 'https://free-ss.site/v/443.json';
+   	$ch = curl_init($url);
+	      
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	$ret = curl_exec($ch);
+	curl_close($ch);
+
+
+        $url = 'https://free-ss.site/v/80.json';
+   	$ch = curl_init($url);
+	      
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	$ret2 = curl_exec($ch);
+	curl_close($ch);
+
+
+
+
+?>
