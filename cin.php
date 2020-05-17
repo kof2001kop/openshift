7e34ee291d5a08b32205193db003374e495d246e
kof2001kop <kof2001kop@163.com>
2020-05-17 17:36:54 +0800

    Update cin.php

--- cin.php
+++ cin.php
@@ -23,9 +23,10 @@
 	$result = curl_exec($ch);
 	curl_close($ch);
 
-	$result = explode("\r\n", $result);
-	print_r ($result);
-	//echo $result;
+	$result = substr($result, 0, strpos($result, "\r\n\r\n"));
+	//$result = explode("\r\n", $result);
+	//print_r ($result);
+	echo $result;
 
 /*__cfduid=d065f3a17b8aa4d2c96642d47accb7c361589704643; expires=Tue, 16-Jun-20 08:37:23 GMT; path=/; domain=.cccat.io; HttpOnly; SameSite=Lax; Secure*/
 ?>
