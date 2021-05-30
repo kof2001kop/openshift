71c46cbab016822b748f4855a245a034d6927d04
kof2001kop <kof2001kop@163.com>
2021-05-30 15:46:05 +0800

    Create translate.php

---
+++ translate.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* For get a api key, contacme in telegram: @Mateodioev
+* free api_key: senkku_bot_public
+*/
+
+header('content-type: application/json');
+define('api_key', 'senkku_bot_public');
+
+function sendPost($link_send, $data_post){
+    $ch = curl_init($link_send);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_post);
+    $res = curl_exec($ch);
+    return $res;
+}
+
+$text = 'Hello world';
+
+$data = sendPost('https://tgneto-space.com/tr/?key='.api_key.'&sl=auto&tl=es', 'text='.$text);
+
+echo $data;
+
+?>
