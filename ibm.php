9a5af063aab99c863209398d6dbe30a86011fe71
kof2001kop <kof2001kop@163.com>
2021-06-08 21:32:06 +0800

    Update ibm.php

--- ibm.php
+++ ibm.php
@@ -1,7 +1,20 @@
 <?php
 
 	$url = 'https://kpr-translte.us-south.cf.appdomain.cloud/test.php'
-	$data = array('translate' => urlencode('how old are you?'));
-	$response = wp_remote_post($url, $data);
-	echo $response;
+	$data = array('translate' => 'how old are you?');
+$data = http_build_query($data);
+ 
+$opts = array (
+'http' => array (
+'method' => 'POST',
+'header'=> 'Content-type: application/x-www-form-urlencodedrn' .
+'Content-Length: ' . strlen($data) . 'rn',
+'content' => $data
+)
+);
+ 
+$context = stream_context_create($opts);
+$html = file_get_contents($url, false, $context);
+ 
+echo $html;
 ?>
