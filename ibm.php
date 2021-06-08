c4f31ce758c4e5c12d76423f779ca540e726714b
kof2001kop <kof2001kop@163.com>
2021-06-08 21:06:59 +0800

    Update ibm.php

--- ibm.php
+++ ibm.php
@@ -3,17 +3,6 @@
 	$url = 'https://kpr-translte.us-south.cf.appdomain.cloud/test.php'
 	$data = array('translate' => 'how old are you?');
 
-	// use key 'http' even if you send the request to https://...
-	$options = array(
-    	'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    	)
-	);
-	$context  = stream_context_create($options);
-	$result = file_get_contents($url, false, $context);
-	if ($result === FALSE) { /* Handle error */ }
-
-	echo $result;
+	$response = wp_remote_post($url, $data);
+	echo $response;
 ?>
