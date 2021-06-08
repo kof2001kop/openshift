3a7f43c5c46f576ef8456c6150cab4a74fed5cf7
kof2001kop <kof2001kop@163.com>
2021-06-08 20:49:39 +0800

    Update and rename pc12.php to ibm.php

---
+++ ibm.php
@@ -0,0 +1,26 @@
+<?php
+
+	$url = 'https://kpr-translte.us-south.cf.appdomain.cloud/test.php'
+
+	//The data you want to send via POST
+	$fields = ['translate'=>'how old are you?'];
+
+	//url-ify the data for the POST
+	$fields_string = http_build_query($fields);
+
+	//open connection
+	$ch = curl_init();
+
+	//set the url, number of POST vars, POST data
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch,CURLOPT_POST, true);
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+	//So that curl_exec returns the contents of the cURL; rather than echoing it
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+	//execute post
+	$result = curl_exec($ch);
+	echo $result;
+
+?>
