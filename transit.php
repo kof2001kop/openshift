63d28f7772185371050eb112b6066b8f58638a59
kof2001kop <kof2001kop@163.com>
2021-06-09 09:12:12 +0800

    Update transit.php

--- transit.php
+++ transit.php
@@ -1,5 +1,5 @@
 <?php
 
-    $html = file_get_contents('https://kpr-translte.us-south.cf.appdomain.cloud/translate_ibm.php?translate='.base64_encode($_POST['translate']), 0, stream_context_create(["http"=>["timeout"=>60]]));
+    $html = file_get_contents('https://kpr-translte.us-south.cf.appdomain.cloud/translate_ibm.php?translate='.base64_encode($_GET['translate']), 0, stream_context_create(["http"=>["timeout"=>60]]));
     echo $html;
 ?>
