5e6370b1299dba241ca87604ad5a698ad86bb79e
kof2001kop <kof2001kop@163.com>
2021-06-11 20:40:58 +0800

    Update transit.php

--- transit.php
+++ transit.php
@@ -1,5 +1,13 @@
 <?php
+    
+    if ($_POST['engine'] == "google")
+    {
+        $html = file_get_contents('https://kpr-flask-thankful-fox.mybluemix.net/?engine=google&translate='.base64_encode($_POST['translate']), 0, stream_context_create(["http"=>["timeout"=>60]]));
+    }
+    else
+    {
+        $html = file_get_contents('https://kpr-flask-thankful-fox.mybluemix.net/?engine=alibaba&translate='.base64_encode($_POST['translate']), 0, stream_context_create(["http"=>["timeout"=>60]]));
+    }
 
-    $html = file_get_contents('https://kpr-translte.us-south.cf.appdomain.cloud/translate_ibm.php?translate='.base64_encode($_POST['translate']), 0, stream_context_create(["http"=>["timeout"=>60]]));
     echo $html;
 ?>
